8866738eb23457e723af6392215129a2655c7c99
wertnvgjnvj <sahil1220.be21@chitkara.edu.in>
2025-02-12 15:36:35 +0530

    lecture-6

---
+++ lec6_120225/index.js
@@ -0,0 +1,45 @@
+// console.log(1);
+// console.log(sumOfTwo(10,5));
+// function sumOfTwo(a,b){
+//   return a+b;
+// }
+// setTimeout(console.log(4), 2000000)
+// var start = 1;
+// var end = 100000000;
+
+// while(start < end){
+//   start++;
+// }
+
+// console.log("End");
+
+// setInterval(console.log(9),4000000);
+
+// functions
+
+//  xy(()=>{
+//   console.log(1);
+// })
+
+//name function
+// function name(){
+//     console.log("name");
+// }
+// name();
+
+// // Arrrow function
+// let add = (a, b) => a + b;
+// console.log(add(3, 2));
+
+
+
+function xyz(){
+    console.log("1");
+}
+
+var abc= function cdf(){
+    console.log("2");
+}
+
+xyz();
+cdf();
